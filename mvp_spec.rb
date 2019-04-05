8298bf42814750139ae1574dc8daa9d0dea57765
Jakub Godawa <jgodawa@appriss.com>
2019-04-05 19:59:28 +0200

    Splitting spec into separate spec cases * Watch out for the shared data!

--- mvp_spec.rb
+++ mvp_spec.rb
@@ -1,3 +1,5 @@
+require_relative 'serial_output_catcher'
+
 class MVPSpec
   attr_reader :app
 
@@ -9,15 +11,35 @@ class MVPSpec
     the_spec =<<~END
     1. MVP (Minimum Viable Product):
       * Get input from user on how they feel:
+        - #{they_are_asked_how_they_feel}
     #{it_gets_input_on_how_the_user_feels}
     END
 
     print the_spec
   end
 
+  def they_are_asked_how_they_feel
+    spec = 'When they run the program they should be asked ' \
+      'to rate their feeling'
+
+    expected_prompt = 'Rate how you feel from 1 to 10: Write a note if you want: '
+    $stdin = StringIO.new("\n\n")
+    $stdout = StringIO.new
+
+    app.run
+
+    prompt = $stdout.string
+    $stdout = STDOUT
+
+    if expected_prompt == prompt
+      "\e[#{32}m#{spec}\e[0m"
+    else
+      "\e[#{31}m#{spec}\e[0m"
+    end
+  end
+
   def it_gets_input_on_how_the_user_feels
     spec =<<-END
-    - When they run the program they should be asked to rate their feeling
     - The gradation is from 1 to 10
     - Each time they are welcome to leave an optional note
     END
@@ -32,7 +54,8 @@ class MVPSpec
 
     $stdout = STDOUT
 
-    feeling = app.store.feelings.first
+    # TODO: teardown!
+    feeling = app.store.feelings.last
     if feeling && feeling[:rate] == rate && feeling[:note] == note
       "\e[#{32}m#{spec}\e[0m"
     else
