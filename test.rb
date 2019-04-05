274ec73a1149eeec468e909c40158e9c7ff99f7b
Jakub Godawa <jgodawa@appriss.com>
2019-04-05 19:14:17 +0200

    Moving from tests to specs, client perspective

--- test.rb
+++ test.rb
@@ -1,6 +1,22 @@
 require_relative 'app'
 
-def test(app)
+def spec(app)
+  mvp_description =<<~END
+  1. MVP (Minimum Viable Product):
+    * Get input from user on how they feel:
+  END
+
+  puts mvp_description
+  it_gets_input_on_how_the_user_feels(app)
+end
+
+def it_gets_input_on_how_the_user_feels(app)
+  spec =<<-END
+    - When they run the program they should be asked to rate their feeling
+    - The gradation is from 1 to 10
+    - Each time they are welcome to leave an optional note
+  END
+
   rate = '5'
   note = 'What a day!'
 
@@ -13,11 +29,11 @@ def test(app)
 
   feeling = app.store.feelings.first
   if feeling && feeling[:rate] == rate && feeling[:note] == note
-    "\e[#{32}mTest passed!\e[0m"
+    "\e[#{32}m#{spec}\e[0m"
   else
-    "\e[#{31}mTest failed...\e[0m"
+    "\e[#{31}m#{spec}\e[0m"
   end
 end
 
 app = App.new
-puts test app
+puts spec app
