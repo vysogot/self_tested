bcca3ac7b9ba6ec8f001907ef2ccb2576b947ce7
Jakub Godawa <jgodawa@appriss.com>
2019-04-05 21:35:03 +0200

    Extract expectation method

--- mvp_spec.rb
+++ mvp_spec.rb
@@ -27,26 +27,30 @@ class MVPSpec
     app.store.clear!
   end
 
+  def expect(received_value, expected_value, spec)
+    if expected_value == received_value
+      "\e[#{32}m#{spec}\e[0m"
+    else
+      "\e[#{31}m#{spec}\e[0m"
+    end
+  end
+
   def they_are_asked_how_they_feel
     teardown!
 
     spec = 'When they run the program they should be asked ' \
       'to rate their feeling'
 
-    expected_prompt = 'Rate how you feel from 1 to 10: ' \
+    prompt = 'Rate how you feel from 1 to 10: ' \
       'Write a note if you want: '
 
-    prompt = catch_output do
+    received_prompt = catch_output do
       mock_input("\n\n") do
         app.run
       end
     end
 
-    if expected_prompt == prompt
-      "\e[#{32}m#{spec}\e[0m"
-    else
-      "\e[#{31}m#{spec}\e[0m"
-    end
+    expect(received_prompt, prompt, spec)
   end
 
   def the_gradation_is_from_1_to_10
@@ -60,12 +64,9 @@ class MVPSpec
       app.run
     end
 
-    feeling = app.store.feelings.first
-    if feeling && feeling[:rate] == rate
-      "\e[#{32}m#{spec}\e[0m"
-    else
-      "\e[#{31}m#{spec}\e[0m"
-    end
+    received_rate = app.store.feelings.first[:rate]
+
+    expect(received_rate, rate, spec)
   end
 
   def they_are_welcome_to_leave_optional_note
@@ -73,20 +74,16 @@ class MVPSpec
 
     spec = 'Each time they are welcome to leave an optional note'
 
-    rate = '5'
     note = 'What a day!'
 
     prompt = catch_output do
-      mock_input("#{rate}\n#{note}\n") do
+      mock_input("\n#{note}\n") do
         app.run
       end
     end
 
-    feeling = app.store.feelings.first
-    if feeling && feeling[:rate] == rate && feeling[:note] == note
-      "\e[#{32}m#{spec}\e[0m"
-    else
-      "\e[#{31}m#{spec}\e[0m"
-    end
+    received_note = app.store.feelings.first[:note]
+
+    expect(received_note, note, spec)
   end
 end
