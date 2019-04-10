eae18dd2fbadef783641f9e304b4333b42dcc5dd
Jakub Godawa <jakub.godawa@gmail.com>
2019-04-10 07:56:01 +0200

    Move mvp specs to a module, add two new failing specs

--- mvp_spec.rb
+++ mvp_spec.rb
@@ -1,6 +1,9 @@
 require_relative 'base_spec'
+require_relative 'mvp_specs'
 
 class MVPSpec < BaseSpec
+  include MVPSpecs
+
   def run
     the_spec =<<~END
     1. MVP (Minimum Viable Product):
@@ -8,57 +11,11 @@ class MVPSpec < BaseSpec
         - #{they_are_asked_how_they_feel}
         - #{the_gradation_is_from_1_to_10}
         - #{they_are_welcome_to_leave_optional_note}
+      * Give stats to the user:
+        - #{they_can_see_their_feelings_stats}
+        - #{they_can_get_daily_weekly_and_monthly_histograms}
     END
 
     print the_spec
-  end
-
-  private
-
-  def they_are_asked_how_they_feel
-    teardown!
-
-    spec = 'When they run the program they should be asked ' \
-      'to rate their feeling'
-    expected_prompt = 'Rate how you feel from 1 to 10: ' \
-      'Write a note if you want: '
-
-    received_prompt = catch_output do
-      mock_input("\n\n") do
-        app.run
-      end
-    end
-
-    expect(received_prompt, expected_prompt, spec)
-  end
-
-  def the_gradation_is_from_1_to_10
-    teardown!
-
-    spec = 'The gradation is from 1 to 10'
-    expected_rate = '1'
-
-    mock_input("#{expected_rate}\n\n") do
-      app.run
-    end
-
-    received_rate = app.store.feelings.first[:rate]
-    expect(received_rate, expected_rate, spec)
-  end
-
-  def they_are_welcome_to_leave_optional_note
-    teardown!
-
-    spec = 'Each time they are welcome to leave an optional note'
-    expected_note = 'What a day!'
-
-    prompt = catch_output do
-      mock_input("\n#{expected_note}\n") do
-        app.run
-      end
-    end
-
-    received_note = app.store.feelings.first[:note]
-    expect(received_note, expected_note, spec)
   end
 end
