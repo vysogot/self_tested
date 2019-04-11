985a4bb6a311eadc8908644c79997958faf359f5
Jakub Godawa <jakub.godawa@gmail.com>
2019-04-11 21:16:33 +0200

    Clean up base spec runner class

--- spec/base_spec_runner.rb
+++ spec/base_spec_runner.rb
@@ -5,64 +5,8 @@ class BaseSpecRunner
 
   attr_reader :app
 
-  class << self
-    attr_reader :suite
-
-    @@suite = []
-    @@nest = 0
-  end
-
   def initialize(app)
     @app = app
-  end
-
-  def call
-    @@suite.each do |example_group|
-      nest = "  " * example_group[:nest]
-      puts nest + example_group[:name]
-      example_group[:specs].each do |spec|
-        spec_result = send(spec.method)
-        color = case spec_result
-                when false
-                  31 # red
-                when true
-                  32 # green
-                else
-                  33 # yellow
-                end
-
-        puts nest + "  - \e[#{color}m#{spec.name}\e[0m"
-      end
-    end
-  end
-
-  def self.context(description, &block)
-    @@suite << { name: description, specs: [], nest: @@nest }
-    @@nest += 1
-    instance_eval(&block)
-    @@nest -= 1
-  end
-
-  class << self
-    alias describe context
-  end
-
-  def self.it(description, &block)
-    @@suite.last[:specs] << Spec.new(
-      name: description,
-      method: define_method(description.tr(' ', '_')) do
-        teardown!
-        instance_eval(&block)
-      end,
-    )
-  end
-
-  def teardown!
-    app.store.clear!
-  end
-
-  def expect(received_value, expected_value)
-    expected_value == received_value
   end
 
   class Spec
@@ -72,6 +16,65 @@ class BaseSpecRunner
       @name = name
       @method = method
     end
+  end
+
+  class << self
+    attr_reader :suite
+
+    @@suite = []
+    @@nest = 0
+
+    def context(description, &block)
+      @@suite << { name: description, specs: [], nest: @@nest }
+      @@nest += 1
+      instance_eval(&block)
+      @@nest -= 1
+    end
+
+    alias describe context
+
+    def it(description, &block)
+      @@suite.last[:specs] << Spec.new(
+        name: description,
+        method: define_method(description.tr(' ', '_')) do
+          instance_eval(&block)
+        end,
+      )
+    end
+  end
+
+  def call
+    @@suite.each do |example_group|
+      nest = "  " * example_group[:nest]
+      puts nest + example_group[:name]
+      example_group[:specs].each do |spec|
+        teardown!
+        result = send(spec.method)
+        print_spec(nest, spec.name, result)
+      end
+    end
+  end
+
+  private
+
+  def print_spec(nest, name, result)
+    color = case result
+            when false
+              31 # red
+            when true
+              32 # green
+            else
+              33 # yellow
+            end
+    puts nest + "  - \e[#{color}m#{name}\e[0m"
+  end
+
+  def teardown!
+    app.store.clear!
+  end
+
+  def expect(received_value, expected_value)
+    expected_value == received_value
   end
 end
 
